3346b117afdbb9e00b21349d508567766482740c
Satyam's Macbook <sunny@Satyams-Macbook.local>
2025-07-15 22:52:37 +0530

    refactor: restructure App.jsx with additional routes and separate admin modules
    - Added new routes in App.jsx to support expanded application flow
    - Created separate folders for admin components and admin pages
    - Improved project structure for better maintainability and scalability

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Layout
+  const logout = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer">
+        <img
+          src={assets.logo}
+          alt=""
+          className="w-32 sm:w-40 cursor-pointer"
+          onClick={() => navigate("/")}
+        />
+        <button
+          onClick={logout}
+          className="relative overflow-hidden flex items-center gap-2 rounded-full text-sm font-semibold cursor-pointer px-10 py-2.5 text-white bg-gradient-to-r from-purple-600 to-indigo-600 shadow-lg transition-all duration-500 ease-in-out transform hover:scale-105 hover:from-orange-400 hover:to-yellow-300 hover:shadow-2xl before:absolute before:inset-0 before:rounded-full before:ring-2 before:ring-orange-300 before:opacity-0 hover:before:opacity-100"
+        >
+          Logout
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
